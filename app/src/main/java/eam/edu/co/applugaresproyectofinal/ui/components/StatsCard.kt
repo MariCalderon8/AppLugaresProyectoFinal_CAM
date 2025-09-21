c56c26299052bc5787b65e30a53bc86244b02948
Blackadder98 <Sboterog99@gmail.com>
2025-09-21 12:50:52 -0500

    FIX card

--- app/src/main/java/eam/edu/co/applugaresproyectofinal/ui/components/StatsCard.kt
+++ app/src/main/java/eam/edu/co/applugaresproyectofinal/ui/components/StatsCard.kt
@@ -1,9 +1,8 @@
 package eam.edu.co.applugaresproyectofinal.ui.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -14,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -26,7 +26,7 @@ fun StatsCard(
     Card(
         modifier = modifier
             .padding(4.dp)
-            .height(100.dp),
+            .heightIn(min = 100.dp),
         shape = RoundedCornerShape(8.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.Transparent
@@ -38,11 +38,11 @@ fun StatsCard(
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(12.dp)
         ) {
             Text(
                 text = count.toString(),
-                fontSize = 26.sp,
+                fontSize = 25.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black
             )
@@ -50,7 +50,10 @@ fun StatsCard(
                 text = label,
                 fontSize = 11.sp,
                 fontWeight = FontWeight.Light,
-                color = Color.Black
+                color = Color.Black,
+                maxLines = 2,
+                softWrap = true,
+                textAlign = TextAlign.Center
             )
         }
     }
