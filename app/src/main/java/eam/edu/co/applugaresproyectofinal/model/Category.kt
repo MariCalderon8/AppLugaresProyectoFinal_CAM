529c6f229915774e1704f2783fbcd221069fd643
Maria Camila Castro Cardona <castro.maria.5191@eam.edu.co>
2025-09-21 17:58:17 -0500

    UPDATE create place

---
+++ app/src/main/java/eam/edu/co/applugaresproyectofinal/model/Category.kt
@@ -0,0 +1,8 @@
+package eam.edu.co.applugaresproyectofinal.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class Category(
+    val name: String,
+    val icon: ImageVector
+)
