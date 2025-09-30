eb0c2119b4a9a1b94a8dc96a141e9bc7ce5e641b
Maria Camila Castro Cardona <castro.maria.5191@eam.edu.co>
2025-09-29 23:31:56 -0500

    UPDATE mapScreen

--- app/src/main/java/eam/edu/co/applugaresproyectofinal/model/Category.kt
+++ app/src/main/java/eam/edu/co/applugaresproyectofinal/model/Category.kt
@@ -1,8 +1,20 @@
 package eam.edu.co.applugaresproyectofinal.model
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Fastfood
+import androidx.compose.material.icons.outlined.LocalCafe
+import androidx.compose.material.icons.outlined.LocalHotel
+import androidx.compose.material.icons.outlined.Museum
+import androidx.compose.material.icons.outlined.Restaurant
 import androidx.compose.ui.graphics.vector.ImageVector
 
-data class Category(
-    val name: String,
+enum class Category(
+    val displayName: String,
     val icon: ImageVector
-)
+) {
+    RESTAURANT("Restaurante", Icons.Outlined.Restaurant),
+    CAFETERIA("Cafetería", Icons.Outlined.LocalCafe),
+    FAST_FOOD("Comida rápida", Icons.Outlined.Fastfood),
+    HOTEL("Hotel", Icons.Outlined.LocalHotel),
+    MUSEUM("Museo", Icons.Outlined.Museum)
+}
