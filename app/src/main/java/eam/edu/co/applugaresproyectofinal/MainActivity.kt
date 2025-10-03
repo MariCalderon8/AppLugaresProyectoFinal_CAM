77c7791624b3678f72391f4fc45309e4a081cebf
Mari_Calderon <calderon.mariluz.8892@eam.edu.co>
2025-10-03 01:47:53 -0500

    uncomment MainActivity

--- app/src/main/java/eam/edu/co/applugaresproyectofinal/MainActivity.kt
+++ app/src/main/java/eam/edu/co/applugaresproyectofinal/MainActivity.kt
@@ -1,57 +1,26 @@
-//package eam.edu.co.applugaresproyectofinal
-//
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.tooling.preview.Preview
-//import eam.edu.co.applugaresproyectofinal.ui.screens.Navigation
-//import eam.edu.co.applugaresproyectofinal.ui.theme.AppLugaresProyectoFinalTheme
-//
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContent {
-//            AppLugaresProyectoFinalTheme {
-//                Navigation()
-//            }
-//        }
-//    }
-//}
-
 package eam.edu.co.applugaresproyectofinal
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import eam.edu.co.applugaresproyectofinal.ui.screens.user.tabs.NewCommentScreen
-import eam.edu.co.applugaresproyectofinal.ui.screens.user.tabs.NewReportScreen
-import eam.edu.co.applugaresproyectofinal.ui.screens.user.tabs.PlaceDetailScreen
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import eam.edu.co.applugaresproyectofinal.ui.screens.Navigation
 import eam.edu.co.applugaresproyectofinal.ui.theme.AppLugaresProyectoFinalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             AppLugaresProyectoFinalTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    // 👇 Aquí mostramos directamente PlaceDetailScreen
-                    PlaceDetailScreen()
-                }
+                Navigation()
             }
         }
     }
